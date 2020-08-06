496f152efb693c6900110813396a332453f18cd8
chilltycho <chilltycho@gmail.com>
2020-08-06 19:41:50 +0800

    11

--- CH2/os/target/riscv64imac-unknown-none-elf/debug/os.d
+++ CH2/os/target/riscv64imac-unknown-none-elf/debug/os.d
@@ -1,1 +1,1 @@
-/home/chill/Documents/CODE/Rcore/CH2/os/target/riscv64imac-unknown-none-elf/debug/os: /home/chill/Documents/CODE/Rcore/CH2/os/src/main.rs
+/home/lic/文档/C/Rcore/CH2/os/target/riscv64imac-unknown-none-elf/debug/os: /home/lic/文档/C/Rcore/CH2/os/src/main.rs
